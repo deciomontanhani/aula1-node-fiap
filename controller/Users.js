af9a342e0daca7ffbe8232c48709959300366026
decio.montanhani <decio.montanhani@iteris.com.br>
2020-04-25 09:37:55 -0300

    conectando a colleção do firebase

--- controller/Users.js
+++ controller/Users.js
@@ -1,6 +1,19 @@
+const UserModel = require('../model/User');
+const userModel = new UserModel();
+
 class Users {
     get(req, res) {
-        res.send(`Eu recebi o parametro ${req.params.id}`);
+        const { id } = req.params;
+
+        userModel.get(id)
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+
+        res.send(`Eu recebi o parametro ${id}`);
     }
 }
 
--- model/User.js
+++ model/User.js
@@ -3,7 +3,13 @@ const BaseModel = require('./BaseModel');
 class Users extends BaseModel {
     constructor() {
         super();
+    }
 
+    get(id) {
+        return this.db
+            .collection('users')
+            .doc(id)
+            .get();
     }
 }
 
